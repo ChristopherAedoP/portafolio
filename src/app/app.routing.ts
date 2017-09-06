0c918d658992da542374156491764554a9cd2344
Christopher Alexander Aedo Pizarro <Christopher.aedo.p@gmail.com>
2017-09-05 22:46:08 -0300

    se modifica menu

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,11 +5,11 @@ import {  AboutComponent,
           PortafolioItemComponent } from './components/index.paginas';
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: PortafolioItemComponent },
 
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const App_Routing = RouterModule.forRoot(app_routes , { useHash: true });
