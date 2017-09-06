210dff0f7ae252599709ec7a13cf494d62bfd873
Christopher Alexander Aedo Pizarro <Christopher.aedo.p@gmail.com>
2017-09-06 00:12:35 -0300

    servicio firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -7,15 +7,29 @@ import { Injectable } from '@angular/core';
 export class InformacionService {
 
   info: any = {} ;
+  sobre_nosotro: any = {} ;
   cargada: boolean = false;
 
   constructor( public http: Http) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+   }
+
+  public carga_info() {
     this.http.get('./../../assets/data/info.pagina.json')
     .subscribe( data => {
       console.log(data.json());
       this.cargada = true;
       this.info = data.json();
     });
-   }
+  }
 
+  public carga_sobre_nosotros() {
+    this.http.get('https://portafolio-1f681.firebaseio.com/equipo.json')
+    .subscribe( data => {
+      console.log(data.json());
+      this.cargada = true;
+      this.sobre_nosotro = data.json();
+    });
+  }
 }
